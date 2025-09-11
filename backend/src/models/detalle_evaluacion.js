366242a5c19ba8359414f5e7d2a7de45c70aab88
mrMcAko <alechacin08@gmail.com>
2025-09-11 12:42:14 -0600

    feat: Agregamos el modelo DetalleEvaluacion con relaciones a Evaluacion y Criterio

---
+++ backend/src/models/detalle_evaluacion.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+
+import { db } from "../config/db.js"
+import Criterio from "./criterios.js";
+import Evaluacion from "./evaluacion.js";
+
+const DetalleEvaluacion = db.define("DetalleEvaluacion", {
+    id_detalle: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, autoIncrement: true 
+    },
+    puntaje: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+}, {
+    tableName: "detalle_evaluaciones",
+    timestamps: false,
+});
+
+DetalleEvaluacion.belongsTo(Evaluacion, { foreignKey: "id_evaluacion" });
+Evaluacion.hasMany(DetalleEvaluacion, { foreignKey: "id_evaluacion" });
+
+DetalleEvaluacion.belongsTo(Criterio, { foreignKey: "id_criterio" });
+Criterio.hasMany(DetalleEvaluacion, { foreignKey: "id_criterio" });
+
+export default DetalleEvaluacion;
