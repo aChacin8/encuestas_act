174ceb3b63c1f0dacd9d69152f39bbdbbdc520a5
mrMcAko <alechacin08@gmail.com>
2025-09-11 12:44:06 -0600

    feat: Agregamos las rutas para las evaluaciones (crearlas y visualizarlas)

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { getDocenteById, getDocentes } from '../handler/docentes.js';
 import { getCriterios, getCriteriosById } from '../handler/criterios.js';
+import { crearEvaluacion, getEvaluacionesByDocente } from '../handler/evaluacion.js';
 
 const router = express.Router()
 
+router.post('/', crearEvaluacion)
+
 router.get ('/docentes', getDocentes)
 router.get ('/docentes/:id_docente', getDocenteById)
+router.get ('/docentes/:id_docente/evaluaciones', getEvaluacionesByDocente)
 
 router.get ('/criterios', getCriterios)
 router.get ('/criterios/:id_criterio', getCriteriosById)
