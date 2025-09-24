ede852f009152008818548dda67ec43281d40cc3
mrMcAko <alechacin08@gmail.com>
2025-09-24 14:38:20 -0600

    feat: Estructura del main del frontend usando query client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBundary.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-  <StrictMode>
-    <ErrorBundary>
-    <App />
-    </ErrorBundary>
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
