edbd332632006fc13cdeb02878a7ea6898ed344d
mrMcAko <alechacin08@gmail.com>
2025-09-24 14:37:35 -0600

    feat: Creamos el frontend, y eliminamos componentes mal hechos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
-import React from "react";
-import DocentesList from "./components/DocenteList";
-import CriteriosList from "./components/Criterios";
+import { BrowserRouter, Routes, Route } from 'react-router';
+import DocentList from './components/DocentList';
+import DocentDetail from './pages/DocentDetail';
 
 function App() {
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6">Evaluaciones</h1>
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4">Docentes</h2>
-        <DocenteList />
-      </section>
-      <section>
-        <h2 className="text-2xl font-semibold mb-4">Criterios</h2>
-        <Criterios />
-      </section>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DocentList />} />
+        <Route path="/docentes/:id" element={<DocentDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
