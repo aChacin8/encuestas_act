64f5d38a56eb234812dcf33ce5dcdbab979213d2
aChacin8 <alechacin08@gmail.com>
2025-10-22 13:40:31 -0600

    feat: Adding route to get specific alumno

--- frontend/src/routes/RoutesIndex.jsx
+++ frontend/src/routes/RoutesIndex.jsx
@@ -1,19 +1,21 @@
-import {Routes, Route} from 'react-router';
+import { Routes, Route } from 'react-router';
 
 import DocenteList from '../components/DocentList';
 import DocentDetail from '../pages/DocentDetail';
+import AlumoDetail from '../pages/AlumnoDetail';
 import SignUp from '../pages/SignUp';
 import Login from '../pages/Login';
 
 const RoutesIndex = () => {
-    return (
-       <Routes>
-        <Route path='/alumnos' element={<SignUp/>}/>
-        <Route path='/alumnos/login' element={<Login/>}/>
-        <Route path="/docentes" element={<DocenteList />} />
-        <Route path="/docentes/:id" element={<DocentDetail />} />
-      </Routes>
-    );
+  return (
+    <Routes>
+      <Route path='/alumnos' element={<SignUp />} />
+      <Route path='/alumnos/login' element={<Login />} />
+      <Route path="/docentes" element={<DocenteList />} />
+      <Route path="/docentes/:id" element={<DocentDetail />} />
+      <Route path='/alumnos/:codigo_estudiante' element={<AlumoDetail />} />
+    </Routes>
+  );
 }
 
 export default RoutesIndex;
