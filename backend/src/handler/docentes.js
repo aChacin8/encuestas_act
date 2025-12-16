2c882dd13deb8136506c927aaefc63cee76ad3f8
aChacin8 <alechacin08@gmail.com>
2025-12-16 17:17:47 -0600

    feat: Agregamos funcion para iniciar sesion como docente, y para obtener los perfiles de los docentes

--- backend/src/handler/docentes.js
+++ backend/src/handler/docentes.js
@@ -1,5 +1,7 @@
+import Alumno from '../models/alumnos.js';
 import Docente from '../models/docentes.js';
 
+import jwt from 'jsonwebtoken';
 export const getDocentes = async (req, res) => {
     try {
         const docentes = await Docente.findAll({where: {activo: true}});
@@ -25,3 +27,51 @@ export const getDocenteById = async (req, res) => {
         return res.status(500).json({msg: err.message});
     }
 }
+
+export const loginDocente = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const docente = await Docente.findOne({where: { email, activo: true }});
+        if (!docente) {
+            return res.status(404).json({ msg: "Docente no encontrado" });
+        }
+
+        // const validPassword = await bcrypt.compare(password, docente.password);
+        const validPassword = password === docente.password;
+        if (!validPassword) {
+            return res.status(401).json({ message: 'password incorrecta' });
+        }
+
+        const token = jwt.sign(
+            {
+                id_docente: docente.id_docente
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '8h' }
+        )
+        return res.status(200).json({
+            msg: "Docente inicio de sesion exitoso",
+            token
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ msg: "Error en el login", error: error.message });
+    }
+}
+
+export const getDocenteProfile = async (req, res) => {
+    try {
+        const docente = await Docente.findByPk(req.id_docente, {
+            attributes: { exclude: ['password'] }
+        });
+        if (!docente) {
+            return res.status(404).json({ msg: "Docente no encontrado" });
+        }
+        return res.status(200).json({ docente });
+    } catch (error) {
+        return res.status(500).json({ msg: "Error al obtener el perfil del docente", error: error.message });
+    }
+}
+
+
