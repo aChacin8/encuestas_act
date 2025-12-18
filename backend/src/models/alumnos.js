c6d0bb159513e1552cb1f8c36f064f9f20da9993
mrMcAko <alechacin08@gmail.com>
2025-12-18 13:32:00 -0600

    feat: Adding email to alumno

--- backend/src/models/alumnos.js
+++ backend/src/models/alumnos.js
@@ -15,6 +15,11 @@ const Alumno = db.define("Alumno", {
         type: DataTypes.STRING(100),
         allowNull: false
     },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
     sede: {
         type: DataTypes.ENUM(
             'UVM Reforma',
